7eb8ccfe746b7e0843b31559a5b0f99b94de491a
Azleeestilon <estilonazlee123@gmail.com>
2026-06-05 12:04:32 +0800

    Fix root route to redirect to login

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::middleware(['guest'])->group(function () {
